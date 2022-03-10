8591fc32f39b37b0d2113688148fa1771a69323b
Freddy <freddy_maupou@orange.fr>
2022-03-10 16:05:43 +0100

    We can now move the lamp with the mouse (thx atan2)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,16 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController PlayerController;
     private SpriteRenderer srPlayer;
-    public Light lampLight;
+    public GameObject lampLight;
 
     public Camera mainCam;
 
     public float speed = 0f;
     public float speedTurn = 0f;
-    public float mouseSensibility;
 
-    private Vector2 mousePos;
-    private float mouseY;
+    private float angle;
     private void Start()
     {
         PlayerController = GetComponent<CharacterController>();
@@ -24,13 +22,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // Move player
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         PlayerController.SimpleMove(movement);
         mainCam.transform.position = new Vector3(mainCam.transform.position.x, transform.position.y + 0.7f, mainCam.transform.position.z);
 
-        mouseY = Input.GetAxis("Mouse Y") * mouseSensibility * Time.deltaTime;
-        lampLight.transform.Rotate(Vector3.up * mouseY); 
+        // Move Light
+        Vector3 toConvert = Input.mousePosition;
+        toConvert.z = -mainCam.transform.position.z;
+        Vector3 mousePos = mainCam.ScreenToWorldPoint(toConvert);
+        angle = Mathf.Atan2(mousePos.y - lampLight.transform.position.y, mousePos.x - lampLight.transform.position.x);
+        lampLight.transform.rotation = Quaternion.Euler(-angle * Mathf.Rad2Deg, 90, 90);
 
+        // Turn player
         if(Input.GetAxis("Horizontal") < 0)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -180, 0), Time.deltaTime * speedTurn);
