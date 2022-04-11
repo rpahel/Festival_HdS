6d0426fd8312420ccfd0feac8ff0081b27fc8878
rpahel <raphael.dacosta.pro@gmail.com>
2022-04-11 20:39:16 +0200

    Placed second Trigger for Infinite Stairs effect

--- Assets/Scripts/InfiniteStairsEffect/InfiniteStairsEffect.cs
+++ Assets/Scripts/InfiniteStairsEffect/InfiniteStairsEffect.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class TeleportHigher : MonoBehaviour
+public class InfiniteStairsEffect : MonoBehaviour
 {
     public float yAmount;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Joueur touché !");
             (other.gameObject.GetComponent<CharacterController>()).enabled = false;
             other.gameObject.transform.position = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y + yAmount, other.gameObject.transform.position.z);
             (other.gameObject.GetComponent<CharacterController>()).enabled = true;
