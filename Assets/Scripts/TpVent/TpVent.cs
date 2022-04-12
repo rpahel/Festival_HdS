0278592e81323ffb1a1db0649069351da5babeef
Freddy <freddy_maupou@orange.fr>
2022-04-12 12:45:45 +0200

    Debug TpVent

--- Assets/Scripts/TpVent/TpVent.cs
+++ Assets/Scripts/TpVent/TpVent.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class TpVent : MonoBehaviour
 {
     public Transform endPoint;
-    private void Start()
-    {
-        Debug.Log(endPoint.position);
-        Debug.Log(endPoint.parent.position);
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Vector3 endPointChanged = new Vector3(endPoint.position.x + 2f, endPoint.position.y, endPoint.position.z);
-            GameObject.FindGameObjectWithTag("Player").transform.position = endPointChanged;
+            Debug.Log(endPointChanged);
+            Debug.Log(other.gameObject);
+            other.transform.position = endPointChanged;
         }
+        Debug.Log(other.gameObject);
     }
 }
